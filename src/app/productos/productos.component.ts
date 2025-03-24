30fe6e6134ced64ae0d6bc9159f3d4b729a8e0a4
Danyy915 <imdany915@gmail.com>
2025-03-23 17:31:59 -0700

    first commi222a1

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -12,26 +12,26 @@ import { RouterLink } from '@angular/router';
 })
 export class ProductosComponent {
   productos: Producto[] = [
-    { "id": 1, "name": "Pizza", "description": "Deliciosa pizza con queso y pepperoni.", "price": 150, "image": "/pizza.jpg", "type": "Snack" },
-    { "id": 2, "name": "Hamburguesa", "description": "Hamburguesa con carne jugosa", "price": 120, "image": "/hamburguesa.jpg", "type": "Snack" },
-    { "id": 3, "name": "Frappe", "description": "Frappe muy bueno.", "price": 90, "image": "/frappe.jpg", "type": "Bebida" },
+    { "id": 1, "name": "Pizza", "description": "Deliciosa pizza con queso y pepperoni.", "price": 150, "image": "/UpsCoffee/pizza.jpg", "type": "Snack" },
+    { "id": 2, "name": "Hamburguesa", "description": "Hamburguesa con carne jugosa", "price": 120, "image": "/UpsCoffee/hamburguesa.jpg", "type": "Snack" },
+    { "id": 3, "name": "Frappe", "description": "Frappe muy bueno.", "price": 90, "image": "/UpsCoffee/frappe.jpg", "type": "Bebida" },
     { "id": 4, "name": "Enchiladas", "description": "Enchiladas del tipo a escoger.", "price": 200, "image": "/UpsCoffee/enchiladas.jpg", "type": "Mexicana" },
-    { "id": 5, "name": "Ensalada", "description": "Ensalada fresca con ingredientes naturales.", "price": 80, "image": "/ensalada.jpg", "type": "Saludable" },
-    { "id": 6, "name": "Torta", "description": "Torta rellena con carne, queso y salsa.", "price": 110, "image": "/torta.jpg", "type": "Mexicana" },
-    { "id": 7, "name": "Chilaquiles", "description": "Chilaquiles con salsa verde o roja, acompañados de pollo o huevo.", "price": 130, "image": "/chilaquiles.jpg", "type": "Mexicana" },
-    { "id": 8, "name": "Tacos", "description": "Tacos al pastor con piña y cebolla.", "price": 70, "image": "/tacos.jpg", "type": "Mexicana" },
-    { "id": 9, "name": "Sopa", "description": "Sopa de pollo con verduras y fideos.", "price": 85, "image": "/sopa.jpg", "type": "Saludable" },
-    { "id": 10, "name": "Huevos", "description": "Huevos estrellados muy ricos.", "price": 45, "image": "/huevos.jpg", "type": "Desayuno" },
-    { "id": 11, "name": "Quesadillas", "description": "Quesadillas con salsa.", "price": 50, "image": "/quesadillas.jpg", "type": "Mexicana" },
-    { "id": 12, "name": "Baguette Pollo", "description": "Baguette de pollo premium muy sabroso.", "price": 90, "image": "/baguette.jpg", "type": "Snack" },
-    { "id": 13, "name": "Coca Cola", "description": "Coca Cola lata 355 ml", "price": 20, "image": "/coca.jpg", "type": "Bebida" },
-    { "id": 14, "name": "Boneless", "description": "Boneless de pollo muy delicioso.", "price": 100, "image": "/boneless.jpg", "type": "Snack" },
-    { "id": 15, "name": "Tostadas", "description": "Tostadas crujientes con guacamole y salsa.", "price": 60, "image": "/tostadas.jpg", "type": "Mexicana" },
-    { "id": 16, "name": "Chocomilk", "description": "Chocomilk cremoso con sabor a vainilla.", "price": 80, "image": "/milkshake.jpg", "type": "Bebida" },
-    { "id": 17, "name": "Ceviche", "description": "Ceviche fresco con camarón y limón.", "price": 180, "image": "/ceviche.jpg", "type": "Mexicana" },
-    { "id": 18, "name": "Nachos", "description": "Nachos con queso fundido y jalapeños.", "price": 90, "image": "/nachos.jpg", "type": "Snack" },
-    { "id": 19, "name": "Café", "description": "Café recién preparado, fuerte y delicioso.", "price": 40, "image": "/cafe.jpg", "type": "Bebida" },
-    { "id": 20, "name": "Tortillas", "description": "Tortillas frescas hechas a mano.", "price": 30, "image": "/tortillas.jpg", "type": "Mexicana" }
+    { "id": 5, "name": "Ensalada", "description": "Ensalada fresca con ingredientes naturales.", "price": 80, "image": "/UpsCoffee/ensalada.jpg", "type": "Saludable" },
+    { "id": 6, "name": "Torta", "description": "Torta rellena con carne, queso y salsa.", "price": 110, "image": "/UpsCoffee/torta.jpg", "type": "Mexicana" },
+    { "id": 7, "name": "Chilaquiles", "description": "Chilaquiles con salsa verde o roja, acompañados de pollo o huevo.", "price": 130, "image": "/UpsCoffee/chilaquiles.jpg", "type": "Mexicana" },
+    { "id": 8, "name": "Tacos", "description": "Tacos al pastor con piña y cebolla.", "price": 70, "image": "/UpsCoffee/tacos.jpg", "type": "Mexicana" },
+    { "id": 9, "name": "Sopa", "description": "Sopa de pollo con verduras y fideos.", "price": 85, "image": "/UpsCoffee/sopa.jpg", "type": "Saludable" },
+    { "id": 10, "name": "Huevos", "description": "Huevos estrellados muy ricos.", "price": 45, "image": "/UpsCoffee/huevos.jpg", "type": "Desayuno" },
+    { "id": 11, "name": "Quesadillas", "description": "Quesadillas con salsa.", "price": 50, "image": "/UpsCoffee/quesadillas.jpg", "type": "Mexicana" },
+    { "id": 12, "name": "Baguette Pollo", "description": "Baguette de pollo premium muy sabroso.", "price": 90, "image": "/UpsCoffee/baguette.jpg", "type": "Snack" },
+    { "id": 13, "name": "Coca Cola", "description": "Coca Cola lata 355 ml", "price": 20, "image": "/UpsCoffee/coca.jpg", "type": "Bebida" },
+    { "id": 14, "name": "Boneless", "description": "Boneless de pollo muy delicioso.", "price": 100, "image": "/UpsCoffee/boneless.jpg", "type": "Snack" },
+    { "id": 15, "name": "Tostadas", "description": "Tostadas crujientes con guacamole y salsa.", "price": 60, "image": "/UpsCoffee/tostadas.jpg", "type": "Mexicana" },
+    { "id": 16, "name": "Chocomilk", "description": "Chocomilk cremoso con sabor a vainilla.", "price": 80, "image": "/UpsCoffee/milkshake.jpg", "type": "Bebida" },
+    { "id": 17, "name": "Ceviche", "description": "Ceviche fresco con camarón y limón.", "price": 180, "image": "/UpsCoffee/ceviche.jpg", "type": "Mexicana" },
+    { "id": 18, "name": "Nachos", "description": "Nachos con queso fundido y jalapeños.", "price": 90, "image": "/UpsCoffee/nachos.jpg", "type": "Snack" },
+    { "id": 19, "name": "Café", "description": "Café recién preparado, fuerte y delicioso.", "price": 40, "image": "/UpsCoffee/cafe.jpg", "type": "Bebida" },
+    { "id": 20, "name": "Tortillas", "description": "Tortillas frescas hechas a mano.", "price": 30, "image": "/UpsCoffee/tortillas.jpg", "type": "Mexicana" }
 ]
 ;
   iconosCategorias: { [key: string]: string } = {
